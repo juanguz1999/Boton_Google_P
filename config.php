d5d7dd07e74c0a5ab4b285f9a543fa53392e7ada
Preytax <100790085+Preytax@users.noreply.github.com>
2022-03-07 17:03:11 -0500

    Boton_Google_P_100%

--- config.php
+++ config.php
@@ -1,28 +1,29 @@
 <?php
 
-//start session on web page
+//iniciar sesión en la página web
 session_start();
 
 //config.php
 
-//Include Google Client Library for PHP autoload file
+//Incluir la biblioteca de clientes de Google para el archivo de carga automática de PHP
 require_once 'vendor/autoload.php';
 
-//Make object of Google API Client for call Google API
+//Hacer objeto de Google API Client para llamar a Google API
 $google_client = new Google_Client();
 
-//Set the OAuth 2.0 Client ID
+//Establecer el ID de cliente de OAuth 2.0
 $google_client->setClientId('30639041895-0l8at7s382j7gkv9ka4th2vp8tjuk2tl.apps.googleusercontent.com');
 
-//Set the OAuth 2.0 Client Secret key
+//Establecer la clave secreta del cliente de OAuth 2.0
 $google_client->setClientSecret('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');
 
-//Set the OAuth 2.0 Redirect URI
+//Establecer el URI de redirección de OAuth 2.0
 $google_client->setRedirectUri('http://localhost/Boton_Google_P/index.php');
 
-// to get the email and profile 
+//para obtener el correo electrónico
 $google_client->addScope('email');
 
+//para obtener el perfil
 $google_client->addScope('profile');
 
-?> 
+?> Close your php here
